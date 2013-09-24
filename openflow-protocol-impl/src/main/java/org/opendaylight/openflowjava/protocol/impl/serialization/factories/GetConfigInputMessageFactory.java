1e439a51ce35e34f6203a64dba532aba9b5999af
Timotej Kubas <timotej.kubas@pantheon.sk>
2013-09-24 10:58:54 +0200

    GetAsyncReplyMessageFactory, QueueGetConfigReplyMessageFactoryTest
    adding new serialization/deserialization factories
    
    Change-Id: Ib39c1c28f4146791d16f1b56d25231ecb61ae3df
    Signed-off-by: Timotej Kubas <timotej.kubas@pantheon.sk>

--- openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/serialization/factories/GetConfigInputMessageFactory.java
+++ openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/serialization/factories/GetConfigInputMessageFactory.java
@@ -3,8 +3,8 @@ package org.opendaylight.openflowjava.protocol.impl.serialization.factories;
 
 import io.netty.buffer.ByteBuf;
 
-import org.opendaylight.openflowjava.protocol.impl.core.OFFrameDecoder;
 import org.opendaylight.openflowjava.protocol.impl.serialization.OFSerializer;
+import org.opendaylight.openflowjava.protocol.impl.util.ByteBufUtils;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.GetConfigInput;
 
 /**
@@ -15,6 +15,7 @@ public class GetConfigInputMessageFactory implements OFSerializer<GetConfigInput
 
     /** Code type of GetConfigRequest message */
     public static final byte MESSAGE_TYPE = 7;
+    private static final int MESSAGE_LENGTH = 8;
     private static GetConfigInputMessageFactory instance;
     
     private GetConfigInputMessageFactory() {
@@ -34,11 +35,17 @@ public class GetConfigInputMessageFactory implements OFSerializer<GetConfigInput
     @Override
     public void messageToBuffer(short version, ByteBuf out,
             GetConfigInput message) {
-        out.writeByte(message.getVersion());
-        out.writeByte(MESSAGE_TYPE);
-        out.writeShort(OFFrameDecoder.LENGTH_OF_HEADER);
-        out.writeInt(message.getXid().intValue());
-        
+        ByteBufUtils.writeOFHeader(instance, message, out);
+    }
+
+    @Override
+    public int computeLength() {
+        return MESSAGE_LENGTH;
+    }
+
+    @Override
+    public byte getMessageType() {
+        return MESSAGE_TYPE;
     }
 
 }
--- openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/serialization/factories/RoleRequestInputMessageFactory.java
+++ openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/serialization/factories/RoleRequestInputMessageFactory.java
@@ -3,7 +3,6 @@ package org.opendaylight.openflowjava.protocol.impl.serialization.factories;
 
 import io.netty.buffer.ByteBuf;
 
-import org.opendaylight.openflowjava.protocol.impl.core.OFFrameDecoder;
 import org.opendaylight.openflowjava.protocol.impl.serialization.OFSerializer;
 import org.opendaylight.openflowjava.protocol.impl.util.ByteBufUtils;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.RoleRequestInput;
@@ -16,6 +15,7 @@ public class RoleRequestInputMessageFactory implements OFSerializer<RoleRequestI
 
     /** Code type of RoleRequest message */
     public static final byte MESSAGE_TYPE = 24;
+    private static final int MESSAGE_LENGTH = 8;
     private static final byte PADDING_IN_ROLE_REQUEST_MESSAGE = 4;
     private static RoleRequestInputMessageFactory instance;
     
@@ -36,13 +36,20 @@ public class RoleRequestInputMessageFactory implements OFSerializer<RoleRequestI
     @Override
     public void messageToBuffer(short version, ByteBuf out,
             RoleRequestInput message) {
-        out.writeByte(message.getVersion());
-        out.writeByte(MESSAGE_TYPE);
-        out.writeShort(OFFrameDecoder.LENGTH_OF_HEADER);
-        out.writeInt(message.getXid().intValue());
+        ByteBufUtils.writeOFHeader(instance, message, out);
         out.writeInt(message.getRole().getIntValue());
         ByteBufUtils.padBuffer(PADDING_IN_ROLE_REQUEST_MESSAGE, out);
         out.writeLong(message.getGenerationId().longValue());
+    }
+
+    @Override
+    public int computeLength() {
+        return MESSAGE_LENGTH;
+    }
+
+    @Override
+    public byte getMessageType() {
+        return MESSAGE_TYPE;
     }
 
 }
