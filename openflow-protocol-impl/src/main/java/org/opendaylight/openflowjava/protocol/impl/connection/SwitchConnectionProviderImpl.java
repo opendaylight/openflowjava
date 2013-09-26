c908c74e68ff5a50ca9d43de5a5a640464c5cb3f
michal.polkorab <michal.polkorab@pantheon.sk>
2013-09-26 09:25:01 +0200

    Added Yang code generation for Multipart messages
    Updated comments
    Removed gitignore
    
    Signed-off-by: michal.polkorab <michal.polkorab@pantheon.sk>
    Change-Id: Ic31c58db37a9dfe072b53eeb64209c5a2d9c9a41

--- openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/connection/SwitchConnectionProviderImpl.java
+++ openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/connection/SwitchConnectionProviderImpl.java
@@ -1,10 +1,4 @@
-/**
- * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
- *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v1.0 which accompanies this distribution,
- * and is available at http://www.eclipse.org/legal/epl-v10.html
- */
+/* Copyright (C)2013 Pantheon Technologies, s.r.o. All rights reserved. */
 
 package org.opendaylight.openflowjava.protocol.impl.connection;
 
@@ -42,7 +36,7 @@ public class SwitchConnectionProviderImpl implements SwitchConnectionProvider {
     public void configure(Collection<ConnectionConfiguration> connConfigs) {
         LOG.debug("Configurating ..");
 
-        //TODO - add and configure servers according to configuration
+        //TODO - configure servers according to configuration
         serverLot = new HashSet<>();
         for (Iterator<ConnectionConfiguration> iterator = connConfigs.iterator(); iterator.hasNext();) {
             ConnectionConfiguration connConfig = iterator.next();
