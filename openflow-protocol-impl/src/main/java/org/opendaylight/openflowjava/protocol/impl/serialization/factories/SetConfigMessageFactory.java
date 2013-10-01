2fbea23a7716c890ebb144f803e88f7b481c720b
michal.polkorab <michal.polkorab@pantheon.sk>
2013-10-01 13:53:04 +0200

    Added Yang models for ofp_match
    Added bitmap of OFPFF_* flags for FlowMod message
    Updated yangtools version dependency
    Fixed new yang parser detected errors
    Added ethertype to actions push pbb/mpls/vlan
    Added hello-elements list
    Added byte[] data field to experimenter
    
    Signed-off-by: michal.polkorab <michal.polkorab@pantheon.sk>
    Change-Id: I4ef33b609db0db833e3c7caa298e555dc33a1a8a

--- openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/serialization/factories/SetConfigMessageFactory.java
+++ openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/serialization/factories/SetConfigMessageFactory.java
@@ -36,8 +36,8 @@ public class SetConfigMessageFactory implements OFSerializer<SetConfigInput> {
     public void messageToBuffer(short version, ByteBuf out,
             SetConfigInput message) {
         ByteBufUtils.writeOFHeader(instance, message, out);
-        // TODO - finish implementation after enums provide needed funcionality
-        out.writeShort(message.getFlags().getIntValue());
+        // TODO - finish implementation after list of enums is generated
+        //out.writeShort(message.getFlags().getIntValue());
         out.writeShort(message.getMissSendLen());
     }
 
