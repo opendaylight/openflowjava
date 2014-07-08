600a545bbfa20e1eed1ce76c28ad6f4048cb6673
Michal Polkorab <michal.polkorab@pantheon.sk>
2014-07-08 18:21:11 +0200

    Example of openflowjava extension for nx_action_resubmit
     - configuration file is located in src/main/resources - use this file to load / register this bundle
     - configuration file is exemplary - it registers the NX_ACTION_RESUBMIT into ofjava 6633 and 6653 instances
     - change contains 2 yang files:
       - nx-resubmit-action.yang - shows how to augment existing structures
       - openflow-nx-resubmit-action.yang - shows how to configure custom bundle
     - unit tests test if the structure is (de)serialized correctly
    
    Change-Id: I6ed7226dba83f36deb100de88e5408ae32c060b2
    Signed-off-by: Michal Polkorab <michal.polkorab@pantheon.sk>

---
+++ openflow-nx-resubmit-action/src/test/java/org/opendaylight/openflowjava/protocol/nx/deserialization/NxActionResubmitDeserializerTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2014 Pantheon Technologies s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowjava.protocol.nx.deserialization;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.UnpooledByteBufAllocator;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.opendaylight.openflowjava.protocol.api.util.EncodeConstants;
+import org.opendaylight.openflowjava.protocol.nx.NxResubmitActionRegistrator;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.augments.rev131002.ExperimenterIdAction;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.action.rev130731.actions.grouping.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.nx.resubmit.action.rev130731.NxActionResubmitAugment;
+
+/**
+ * @author michal.polkorab
+ *
+ */
+public class NxActionResubmitDeserializerTest {
+
+    /**
+     * Test deserialization of NX_ACTION_RESUBMIT action
+     */
+    @Test
+    public void test() {
+        ByteBuf buffer = UnpooledByteBufAllocator.DEFAULT.buffer();
+        buffer.writeShort(EncodeConstants.EXPERIMENTER_VALUE);
+        buffer.writeShort(16);
+        buffer.writeInt(NxResubmitActionRegistrator.NICIRA_EXPERIMENTER_ID.intValue());
+        buffer.writeShort(1);
+        buffer.writeShort(42);
+        buffer.writeByte(5);
+        buffer.writeZero(3);
+
+        NxActionResubmitDeserializer deserializer = new NxActionResubmitDeserializer();
+        Action action = deserializer.deserialize(buffer);
+
+        Assert.assertEquals("Wrong action type", "org.opendaylight.yang.gen.v1.urn.opendaylight"
+                + ".openflow.common.action.rev130731.Experimenter", action.getType().getName());
+        ExperimenterIdAction expAction = action.getAugmentation(ExperimenterIdAction.class);
+        Assert.assertEquals("Wrong vendor id", NxResubmitActionRegistrator.NICIRA_EXPERIMENTER_ID.intValue(),
+                expAction.getExperimenter().getValue().intValue());
+        NxActionResubmitAugment nxResubmit = action.getAugmentation(NxActionResubmitAugment.class);
+        Assert.assertEquals("Wrong subtype", 1, nxResubmit.getSubtype().intValue());
+        Assert.assertEquals("Wrong in-port", 42, nxResubmit.getInPort().intValue());
+        Assert.assertEquals("Wrong table", 5, nxResubmit.getTable().intValue());
+        Assert.assertTrue("Unread data", buffer.readableBytes() == 0);
+    }
+}
