6d2da9d8624db97b64fb4829555e0eb5f9361097
Martin Sunal <msunal@cisco.com>
2014-07-14 14:27:09 +0200

    Nicira extensions in OpenFlowJava library
    - creates and registers codecs(serializers & deserializers) for:
    	actions:
    		reg-load
    		reg-move
    	matches:
    		reg 0-7
    		tun-id
    
    Change-Id: If847b9098722337318109d3ffe34ddaca8b6b363
    Signed-off-by: Martin Sunal <msunal@cisco.com>

---
+++ openflow-extension-nicira/src/main/java/org/opendaylight/openflowjava/nx/codec/action/ActionRegLoadCodec.java
@@ -0,0 +1,33 @@
+package org.opendaylight.openflowjava.nx.codec.action;
+
+import io.netty.buffer.ByteBuf;
+
+import java.math.BigInteger;
+
+import org.opendaylight.openflowjava.protocol.api.extensibility.OFDeserializer;
+import org.opendaylight.openflowjava.protocol.api.extensibility.OFSerializer;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.groupbasedpolicy.of.extension.nicira.action.rev140421.ofj.nx.action.reg.load.grouping.ActionRegLoad;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.groupbasedpolicy.of.extension.nicira.action.rev140421.ofj.nx.action.reg.load.grouping.ActionRegLoadBuilder;
+
+public class ActionRegLoadCodec implements OFSerializer<ActionRegLoad>, OFDeserializer<ActionRegLoad> {
+
+    public static final int LENGTH = 24;
+    public static final byte SUBTYPE = 7; // NXAST_REG_LOAD
+
+    @Override
+    public void serialize(ActionRegLoad input, ByteBuf outBuffer) {
+        outBuffer.writeShort(input.getOfsNbits());
+        outBuffer.writeInt(input.getDst().intValue());
+        outBuffer.writeLong(input.getValue().longValue());
+    }
+
+    @Override
+    public ActionRegLoad deserialize(ByteBuf message) {
+        ActionRegLoadBuilder actionRegLoadBuilder = new ActionRegLoadBuilder();
+        actionRegLoadBuilder.setOfsNbits(message.readUnsignedShort());
+        actionRegLoadBuilder.setDst(message.readUnsignedInt());
+        actionRegLoadBuilder.setValue(BigInteger.valueOf(message.readLong()));
+        return actionRegLoadBuilder.build();
+    }
+
+}
