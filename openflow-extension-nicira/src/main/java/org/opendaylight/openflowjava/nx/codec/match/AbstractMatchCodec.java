81bf0c82b09c4358ad275498f3f10883216fe607
Martin Sunal <msunal@cisco.com>
2014-07-14 14:27:09 +0200

    Nicira extensions in OpenFlowJava library
    - creates and registers codecs(serializers & deserializers) for:
    	actions:
    		reg-load
    		reg-move
    	matches:
    		NXM_NX_REG(0-7)
    		NXM_NX_TUN_ID
    		NXM_NX_ARP_SHA
    		NXM_NX_ARP_THA
    		NXM_OF_ARP_OP
    		NXM_OF_ARP_SPA
    		NXM_OF_ARP_TPA
    		NXM_NX_TUN_IPV4_DST
    		NXM_NX_TUN_IPV4_SRC
    		NXM_OF_ETH_SRC
    		NXM_OF_ETH_DST
    
    Change-Id: If847b9098722337318109d3ffe34ddaca8b6b363
    Signed-off-by: Martin Sunal <msunal@cisco.com>

---
+++ openflow-extension-nicira/src/main/java/org/opendaylight/openflowjava/nx/codec/match/AbstractMatchCodec.java
@@ -0,0 +1,68 @@
+package org.opendaylight.openflowjava.nx.codec.match;
+
+import io.netty.buffer.ByteBuf;
+
+import org.opendaylight.openflowjava.nx.NiciraConstants;
+import org.opendaylight.openflowjava.protocol.api.extensibility.OFDeserializer;
+import org.opendaylight.openflowjava.protocol.api.extensibility.OFSerializer;
+import org.opendaylight.openflowjava.protocol.api.util.EncodeConstants;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.augments.rev131002.ExperimenterIdMatchEntry;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.augments.rev131002.ExperimenterIdMatchEntryBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.types.rev130731.ExperimenterId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev130731.MatchField;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev130731.OxmClassBase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev130731.oxm.fields.grouping.MatchEntries;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev130731.oxm.fields.grouping.MatchEntriesBuilder;
+
+public abstract class AbstractMatchCodec implements OFSerializer<MatchEntries>, OFDeserializer<MatchEntries> {
+
+    protected MatchEntriesBuilder deserializeHeader(ByteBuf message) {
+        MatchEntriesBuilder builder = new MatchEntriesBuilder();
+        builder.setOxmClass(getOxmClass());
+        // skip oxm_class - provided
+        message.skipBytes(EncodeConstants.SIZE_OF_SHORT_IN_BYTES);
+        builder.setOxmMatchField(getNxmField());
+        boolean hasMask = (message.readUnsignedByte() & 1) != 0;
+        builder.setHasMask(hasMask);
+        // skip match entry length - not needed
+        message.skipBytes(EncodeConstants.SIZE_OF_BYTE_IN_BYTES);
+        ExperimenterIdMatchEntryBuilder experimenterIdMatchEntryBuilder = new ExperimenterIdMatchEntryBuilder();
+        experimenterIdMatchEntryBuilder.setExperimenter(new ExperimenterId(NiciraConstants.NX_VENDOR_ID));
+        builder.addAugmentation(ExperimenterIdMatchEntry.class, experimenterIdMatchEntryBuilder.build());
+        return builder;
+    }
+
+    protected void serializeHeader(MatchEntries input, ByteBuf outBuffer) {
+        outBuffer.writeInt(serializeHeaderToLong(input.isHasMask()).intValue());
+    }
+
+    public Long serializeHeaderToLong(boolean hasMask) {
+        return ((((long) getOxmClassCode()) << 16) | (getNxmFieldCode() << 9) | ((hasMask ? 1 : 0) << 8) | (getValueLength()));
+    }
+
+    /**
+     * @return numeric representation of nxm_field
+     */
+    public abstract int getNxmFieldCode();
+
+    /**
+     * @return numeric representation of oxm_class
+     */
+    public abstract int getOxmClassCode();
+
+    /**
+     * @return match entry value length
+     */
+    public abstract int getValueLength();
+
+    /**
+     * @return nxm_field class
+     */
+    public abstract Class<? extends MatchField> getNxmField();
+
+    /**
+     * @return oxm_class class
+     */
+    public abstract Class<? extends OxmClassBase> getOxmClass();
+
+}
