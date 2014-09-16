42517b13c7b53f7b4cb27f7fce43c8addcbb794d
Michal Polkorab <michal.polkorab@pantheon.sk>
2014-09-16 14:28:54 +0200

    Added unit tests
    Change-Id: I1b6b771d5748d348e8420d33ee05676dbac4fed3
    Signed-off-by: Michal Polkorab <michal.polkorab@pantheon.sk>

---
+++ openflow-protocol-impl/src/test/java/org/opendaylight/openflowjava/protocol/impl/connection/MessageListenerWrapperTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2014 Pantheon Technologies s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowjava.protocol.impl.connection;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.HelloInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.HelloInputBuilder;
+
+/**
+ * @author michal.polkorab
+ *
+ */
+public class MessageListenerWrapperTest {
+
+    /**
+     * Test MessageListenerWrapper creation
+     */
+    @Test
+    public void test() {
+        HelloInputBuilder builder = new HelloInputBuilder();
+        HelloInput hello = builder.build();
+        SimpleRpcListener listener = new SimpleRpcListener(hello, "Error");
+        MessageListenerWrapper wrapper = new MessageListenerWrapper(hello, listener);
+        Assert.assertEquals("Wrong message", hello, wrapper.getMsg());
+        Assert.assertEquals("Wrong listener", listener, wrapper.getListener());
+    }
+}
