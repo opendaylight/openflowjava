548217b93ab731a30a2fc883c11c87b43f044b3b
Marian Adamjak <marian.adamjak@pantheon.sk>
2014-10-16 15:21:52 +0200

    Add unit tests for ExperimenterIdSerializerKey, ExperimenterIdDeserializerKey
    Change-Id: Iaf2ad17a1b6ceee6ed1ec040cb12943f0964f582
    Signed-off-by: Marian Adamjak <marian.adamjak@pantheon.sk>

--- openflow-protocol-api/src/test/java/org/opendaylight/openflowjava/protocol/api/keys/experimenter/ExperimenterIdDeserializerKeyTest.java
+++ openflow-protocol-api/src/test/java/org/opendaylight/openflowjava/protocol/api/keys/experimenter/ExperimenterIdDeserializerKeyTest.java
@@ -10,6 +10,7 @@ package org.opendaylight.openflowjava.protocol.api.keys.experimenter;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.opendaylight.openflowjava.protocol.api.extensibility.MessageCodeKey;
 import org.opendaylight.openflowjava.protocol.api.util.EncodeConstants;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.ErrorMessage;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.ExperimenterMessage;
@@ -47,23 +48,20 @@ public class ExperimenterIdDeserializerKeyTest {
         Assert.assertFalse("Wrong equals", key1.equals(key2));
         Assert.assertFalse("Wrong hashcode", key1.hashCode() == key2.hashCode());
     }
-    
+
     /**
      * Test ExperimenterIdDeserializerKey equals - additional test
      */
     @Test
     public void testEquals() {
-        
          ExperimenterIdDeserializerKey key1 =
                  new ExperimenterIdDeserializerKey(EncodeConstants.OF10_VERSION_ID, null, ExperimenterMessage.class);
-         
          Assert.assertTrue("Wrong equal to identical object.", key1.equals(key1));
-         Assert.assertFalse("Wrong equal to different class.", key1.equals(new Object()));
-         
+         MessageCodeKey mk = new MessageCodeKey(EncodeConstants.OF10_VERSION_ID,EncodeConstants.EXPERIMENTER_VALUE, ExperimenterMessage.class);
+         Assert.assertFalse("Wrong equal to different class.", key1.equals(mk));
          ExperimenterIdDeserializerKey key2 =
                  new ExperimenterIdDeserializerKey(EncodeConstants.OF10_VERSION_ID, 42L, ExperimenterMessage.class);
          Assert.assertFalse("Wrong equal by experimenterId.", key1.equals(key2));
-        
     }
-    
+
 }
