2e1513dc13aabdfde86e71a9ddbe1352c657a0ff
Marian Adamjak <marian.adamjak@pantheon.sk>
2014-10-23 14:03:26 +0200

    BUG-2245 Sonar technical debt    - correct Local Final Variable Name
    Change-Id: I4fde8e15edc8893fd9871a268465eb39da5b92b9
    Signed-off-by: Marian Adamjak <marian.adamjak@pantheon.sk>

--- openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/deserialization/factories/PortStatusMessageFactory.java
+++ openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/deserialization/factories/PortStatusMessageFactory.java
@@ -58,38 +58,38 @@ public class PortStatusMessageFactory implements OFDeserializer<PortStatusMessag
     }
 
     private static PortFeatures createPortFeatures(long input){
-        final Boolean _10mbHd = ((input) & (1<<0)) != 0;
-        final Boolean _10mbFd = ((input) & (1<<1)) != 0;
-        final Boolean _100mbHd = ((input) & (1<<2)) != 0;
-        final Boolean _100mbFd = ((input) & (1<<3)) != 0;
-        final Boolean _1gbHd = ((input) & (1<<4)) != 0;
-        final Boolean _1gbFd = ((input) & (1<<5)) != 0;
-        final Boolean _10gbFd = ((input) & (1<<6)) != 0;
-        final Boolean _40gbFd = ((input) & (1<<7)) != 0;
-        final Boolean _100gbFd = ((input) & (1<<8)) != 0;
-        final Boolean _1tbFd = ((input) & (1<<9)) != 0;
-        final Boolean _other = ((input) & (1<<10)) != 0;
-        final Boolean _copper = ((input) & (1<<11)) != 0;
-        final Boolean _fiber = ((input) & (1<<12)) != 0;
-        final Boolean _autoneg = ((input) & (1<<13)) != 0;
-        final Boolean _pause = ((input) & (1<<14)) != 0;
-        final Boolean _pauseAsym = ((input) & (1<<15)) != 0;
-        return new PortFeatures(_100gbFd, _100mbFd, _100mbHd, _10gbFd, _10mbFd, _10mbHd, _1gbFd,
-                _1gbHd, _1tbFd, _40gbFd, _autoneg, _copper, _fiber, _other, _pause, _pauseAsym);
+        final Boolean pf10mbHd = ((input) & (1<<0)) != 0;
+        final Boolean pf10mbFd = ((input) & (1<<1)) != 0;
+        final Boolean pf100mbHd = ((input) & (1<<2)) != 0;
+        final Boolean pf100mbFd = ((input) & (1<<3)) != 0;
+        final Boolean pf1gbHd = ((input) & (1<<4)) != 0;
+        final Boolean pf1gbFd = ((input) & (1<<5)) != 0;
+        final Boolean pf10gbFd = ((input) & (1<<6)) != 0;
+        final Boolean pf40gbFd = ((input) & (1<<7)) != 0;
+        final Boolean pf100gbFd = ((input) & (1<<8)) != 0;
+        final Boolean pf1tbFd = ((input) & (1<<9)) != 0;
+        final Boolean pfOther = ((input) & (1<<10)) != 0;
+        final Boolean pfCopper = ((input) & (1<<11)) != 0;
+        final Boolean pfFiber = ((input) & (1<<12)) != 0;
+        final Boolean pfAutoneg = ((input) & (1<<13)) != 0;
+        final Boolean pfPause = ((input) & (1<<14)) != 0;
+        final Boolean pfPauseAsym = ((input) & (1<<15)) != 0;
+        return new PortFeatures(pf100gbFd, pf100mbFd, pf100mbHd, pf10gbFd, pf10mbFd, pf10mbHd, pf1gbFd,
+                pf1gbHd, pf1tbFd, pf40gbFd, pfAutoneg, pfCopper, pfFiber, pfOther, pfPause, pfPauseAsym);
     }
     
     private static PortState createPortState(long input){
-        final Boolean _linkDown = ((input) & (1<<0)) != 0;
-        final Boolean _blocked  = ((input) & (1<<1)) != 0;
-        final Boolean _live     = ((input) & (1<<2)) != 0;
-        return new PortState(_blocked, _linkDown, _live);
+        final Boolean psLinkDown = ((input) & (1<<0)) != 0;
+        final Boolean psBblocked  = ((input) & (1<<1)) != 0;
+        final Boolean psLive     = ((input) & (1<<2)) != 0;
+        return new PortState(psBblocked, psLinkDown, psLive);
     }
     
     private static PortConfig createPortConfig(long input){
-        final Boolean _portDown   = ((input) & (1<<0)) != 0;
-        final Boolean _noRecv    = ((input) & (1<<2)) != 0;
-        final Boolean _noFwd       = ((input) & (1<<5)) != 0;
-        final Boolean _noPacketIn = ((input) & (1<<6)) != 0;
-        return new PortConfig(_noFwd, _noPacketIn, _noRecv, _portDown);
+        final Boolean pcPortDown   = ((input) & (1<<0)) != 0;
+        final Boolean pcNoRecv    = ((input) & (1<<2)) != 0;
+        final Boolean pcNoFwd       = ((input) & (1<<5)) != 0;
+        final Boolean pcNoPacketIn = ((input) & (1<<6)) != 0;
+        return new PortConfig(pcNoFwd, pcNoPacketIn, pcNoRecv, pcPortDown);
     }
 }
