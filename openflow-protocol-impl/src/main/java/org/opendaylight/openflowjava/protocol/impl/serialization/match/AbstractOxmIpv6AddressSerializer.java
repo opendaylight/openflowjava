e3d8bb156fbc09f6e486a21bac9baa41d5bfad87
Michal Polkorab <michal.polkorab@pantheon.sk>
2015-03-19 11:02:06 +0100

    Bug 2866 - Fixed ipv6 address parsing
     - also added uni test with missing case
    
    Change-Id: I4b0e15f1a1896ed63bc47ce326afea40ff18cc07
    Signed-off-by: Michal Polkorab <michal.polkorab@pantheon.sk>

--- openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/serialization/match/AbstractOxmIpv6AddressSerializer.java
+++ openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/serialization/match/AbstractOxmIpv6AddressSerializer.java
@@ -9,16 +9,10 @@ package org.opendaylight.openflowjava.protocol.impl.serialization.match;
 
 import io.netty.buffer.ByteBuf;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import org.opendaylight.openflowjava.util.ByteBufUtils;
-import org.opendaylight.openflowjava.protocol.api.util.EncodeConstants;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.augments.rev131002.Ipv6AddressMatchEntry;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev130731.oxm.fields.grouping.MatchEntries;
 
-import com.google.common.collect.Lists;
+import com.google.common.net.InetAddresses;
 
 /**
  * Parent for Ipv6 address based match entry serializers
@@ -30,54 +24,12 @@ public abstract class AbstractOxmIpv6AddressSerializer extends AbstractOxmMatchE
     public void serialize(final MatchEntries entry, final ByteBuf outBuffer) {
         super.serialize(entry, outBuffer);
         String textAddress = entry.getAugmentation(Ipv6AddressMatchEntry.class).getIpv6Address().getValue();
-        List<String> address;
-        if (textAddress.equals("::")) {
-            String[] tmp = new String[EncodeConstants.GROUPS_IN_IPV6_ADDRESS];
-            Arrays.fill(tmp, "0");
-            address = Arrays.asList(tmp);
+        if (InetAddresses.isInetAddress(textAddress)) {
+            byte[] binaryAddress = InetAddresses.forString(textAddress).getAddress();
+            outBuffer.writeBytes(binaryAddress);
         } else {
-            address = parseIpv6Address(Lists.newArrayList(ByteBufUtils.COLON_SPLITTER.split(textAddress)));
-        }
-        for (String group : address) {
-            outBuffer.writeShort(Integer.parseInt(group, 16));
+            throw new IllegalArgumentException("Invalid ipv6 address received: " + textAddress);
         }
         writeMask(entry, outBuffer, getValueLength());
-    }
-
-    private static List<String> parseIpv6Address(final ArrayList<String> addressGroups) {
-        int countEmpty = 0;
-        for (String group : addressGroups) {
-            if (group.equals("")) {
-                countEmpty++;
-            }
-        }
-        List<String> ready = new ArrayList<>(EncodeConstants.GROUPS_IN_IPV6_ADDRESS);
-        switch (countEmpty) {
-        case 0:
-            ready = addressGroups;
-            break;
-        case 1:
-            int zerosToBePushed = EncodeConstants.GROUPS_IN_IPV6_ADDRESS - addressGroups.size() + 1;
-            for (String group : addressGroups) {
-                if (group.equals("")) {
-                    for (int j = 0; j < zerosToBePushed; j++) {
-                        ready.add("0");
-                    }
-                } else {
-                    ready.add(group);
-                }
-            }
-            break;
-        case 2:
-            String[] tmp = new String[EncodeConstants.GROUPS_IN_IPV6_ADDRESS];
-            Arrays.fill(tmp, "0");
-            tmp[EncodeConstants.GROUPS_IN_IPV6_ADDRESS - 1] =
-                    addressGroups.get(addressGroups.size() - 1);
-            ready = Arrays.asList(tmp);
-            break;
-        default:
-            throw new IllegalStateException("Incorrect ipv6 address");
-        }
-        return ready;
     }
 }
---
+++ openflow-protocol-impl/src/test/java/org/opendaylight/openflowjava/protocol/impl/serialization/match/OxmIpv6SrcSerializerTest.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) 2015 Pantheon Technologies s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.openflowjava.protocol.impl.serialization.match;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.PooledByteBufAllocator;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.opendaylight.openflowjava.protocol.api.util.EncodeConstants;
+import org.opendaylight.openflowjava.protocol.api.util.OxmMatchConstants;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Ipv6Address;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.augments.rev131002.Ipv6AddressMatchEntry;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.augments.rev131002.Ipv6AddressMatchEntryBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.augments.rev131002.MaskMatchEntry;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.augments.rev131002.MaskMatchEntryBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev130731.Ipv6Src;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev130731.OpenflowBasicClass;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.oxm.rev130731.oxm.fields.grouping.MatchEntriesBuilder;
+
+/**
+ * @author michal.polkorab
+ *
+ */
+public class OxmIpv6SrcSerializerTest {
+
+    OxmIpv6SrcSerializer serializer = new OxmIpv6SrcSerializer();
+
+    /**
+     * Test correct serialization
+     */
+    @Test
+    public void testSerializeWithoutMask() {
+        MatchEntriesBuilder builder = prepareMatchEntry(false, "aaaa:bbbb:1111:2222::");
+
+        ByteBuf buffer = PooledByteBufAllocator.DEFAULT.buffer();
+        serializer.serialize(builder.build(), buffer);
+
+        checkHeader(buffer, false);
+        Assert.assertEquals("Wrong ipv6 address", 43690, buffer.readUnsignedShort());
+        Assert.assertEquals("Wrong ipv6 address", 48059, buffer.readUnsignedShort());
+        Assert.assertEquals("Wrong ipv6 address", 4369, buffer.readUnsignedShort());
+        Assert.assertEquals("Wrong ipv6 address", 8738, buffer.readUnsignedShort());
+        Assert.assertEquals("Wrong ipv6 address", 0, buffer.readUnsignedShort());
+        Assert.assertEquals("Wrong ipv6 address", 0, buffer.readUnsignedShort());
+        Assert.assertEquals("Wrong ipv6 address", 0, buffer.readUnsignedShort());
+        Assert.assertEquals("Wrong ipv6 address", 0, buffer.readUnsignedShort());
+        assertTrue("Unexpected data", buffer.readableBytes() == 0);
+    }
+
+    /**
+     * Test correct serialization
+     */
+    @Test
+    public void testSerializeWithLeadingZeros() {
+        MatchEntriesBuilder builder = prepareMatchEntry(false, "::aaaa:bbbb:1111:2222");
+
+        ByteBuf buffer = PooledByteBufAllocator.DEFAULT.buffer();
+        serializer.serialize(builder.build(), buffer);
+
+        checkHeader(buffer, false);
+        Assert.assertEquals("Wrong ipv6 address", 0, buffer.readUnsignedShort());
+        Assert.assertEquals("Wrong ipv6 address", 0, buffer.readUnsignedShort());
+        Assert.assertEquals("Wrong ipv6 address", 0, buffer.readUnsignedShort());
+        Assert.assertEquals("Wrong ipv6 address", 0, buffer.readUnsignedShort());
+        Assert.assertEquals("Wrong ipv6 address", 43690, buffer.readUnsignedShort());
+        Assert.assertEquals("Wrong ipv6 address", 48059, buffer.readUnsignedShort());
+        Assert.assertEquals("Wrong ipv6 address", 4369, buffer.readUnsignedShort());
+        Assert.assertEquals("Wrong ipv6 address", 8738, buffer.readUnsignedShort());
+        assertTrue("Unexpected data", buffer.readableBytes() == 0);
+    }
+
+    private static MatchEntriesBuilder prepareMatchEntry(boolean hasMask, String value) {
+        MatchEntriesBuilder builder = prepareHeader(hasMask);
+        if (hasMask) {
+            MaskMatchEntryBuilder maskBuilder = new MaskMatchEntryBuilder();
+            maskBuilder.setMask(new byte[]{15, 15, 0, 0});
+            builder.addAugmentation(MaskMatchEntry.class, maskBuilder.build());
+        }
+        Ipv6AddressMatchEntryBuilder addressBuilder = new Ipv6AddressMatchEntryBuilder();
+        addressBuilder.setIpv6Address(new Ipv6Address(value));
+        builder.addAugmentation(Ipv6AddressMatchEntry.class, addressBuilder.build());
+        return builder;
+    }
+
+    private static MatchEntriesBuilder prepareHeader(boolean hasMask) {
+        MatchEntriesBuilder builder = new MatchEntriesBuilder();
+        builder.setOxmClass(OpenflowBasicClass.class);
+        builder.setOxmMatchField(Ipv6Src.class);
+        builder.setHasMask(hasMask);
+        return builder;
+    }
+
+    private static void checkHeader(ByteBuf buffer, boolean hasMask) {
+        assertEquals("Wrong oxm-class", OxmMatchConstants.OPENFLOW_BASIC_CLASS, buffer.readUnsignedShort());
+        short fieldAndMask = buffer.readUnsignedByte();
+        assertEquals("Wrong oxm-field", OxmMatchConstants.IPV6_SRC, fieldAndMask >>> 1);
+        assertEquals("Wrong hasMask", hasMask, (fieldAndMask & 1) != 0);
+        if (hasMask) {
+            assertEquals("Wrong length", 2 * EncodeConstants.SIZE_OF_IPV6_ADDRESS_IN_BYTES,
+                    buffer.readUnsignedByte());
+        } else {
+            assertEquals("Wrong length", EncodeConstants.SIZE_OF_IPV6_ADDRESS_IN_BYTES, buffer.readUnsignedByte());
+        }
+    }
+}
