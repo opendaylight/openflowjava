4f44c7edbe84e6a31a4e0a8887d09b30e34e8dfc
Robert Varga <rovarga@cisco.com>
2015-05-13 01:21:13 +0200

    Move encoding out of flush path
    We want to make sure the flusher thread (at most one per channel) is as
    light as possible. What can do easily is move message encoding into
    OutboundQueue.commit() path, thus charding the caller with serialization
    overhead. We can get most benefit if there are multiple threads
    attempting to access the same device (perhaps on a per-table basis).
    
    Change-Id: I9d198c9eb318f346e129d178f5797a65a97d27aa
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/core/connection/OutboundQueueEntry.java
+++ openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/core/connection/OutboundQueueEntry.java
@@ -7,32 +7,83 @@
  */
 package org.opendaylight.openflowjava.protocol.impl.core.connection;
 
+import com.google.common.base.MoreObjects;
 import com.google.common.base.Preconditions;
 import com.google.common.util.concurrent.FutureCallback;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.buffer.PooledByteBufAllocator;
+import io.netty.channel.socket.DatagramPacket;
+import java.net.InetSocketAddress;
+import org.opendaylight.openflowjava.protocol.impl.serialization.SerializationFactory;
+import org.opendaylight.openflowjava.statistics.CounterEventTypes;
+import org.opendaylight.openflowjava.statistics.StatisticsCounters;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.BarrierInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.FlowModInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.MultipartReplyMessage;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.OfHeader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 final class OutboundQueueEntry {
+    private static final StatisticsCounters COUNTERS = StatisticsCounters.getInstance();
     private static final Logger LOG = LoggerFactory.getLogger(OutboundQueueEntry.class);
+    private static final ByteBufAllocator BUFFER_ALLOCATOR = new PooledByteBufAllocator(false);
+    private final ByteBuf buffer = BUFFER_ALLOCATOR.buffer();
     private FutureCallback<OfHeader> callback;
+    private Object pdu;
     private OfHeader message;
     private boolean completed;
     private volatile boolean committed;
 
-    void commit(final OfHeader message, final FutureCallback<OfHeader> callback) {
+    void commit(final SerializationFactory factory, final InetSocketAddress address, final OfHeader message, final FutureCallback<OfHeader> callback) {
+        if (message == null) {
+            committed = true;
+            return;
+        }
+
+        try {
+            factory.messageToBuffer(message.getVersion(), buffer, message);
+        } catch (Exception e) {
+            LOG.info("Failed to serialize message {}", message, e);
+            COUNTERS.incrementCounter(CounterEventTypes.DS_ENCODE_FAIL);
+            callback.onFailure(e);
+            buffer.clear();
+            committed = true;
+            return;
+        }
+
+        if (message instanceof FlowModInput){
+            COUNTERS.incrementCounter(CounterEventTypes.DS_FLOW_MODS_SENT);
+        }
+        COUNTERS.incrementCounter(CounterEventTypes.DS_ENCODE_SUCCESS);
+
+        final Object encoded;
+        if (address == null) {
+            encoded = buffer;
+        } else {
+            encoded = new DatagramPacket(buffer, address);
+        }
+
         this.message = message;
         this.callback = callback;
+        pdu = encoded;
         committed = true;
     }
 
     void reset() {
+        buffer.clear();
         callback = null;
-        message = null;
-        committed = false;
         completed = false;
+        message = null;
+        pdu = null;
+
+        // volatile, needs to be last for safety reasons
+        committed = false;
+    }
+
+    Object getPdu() {
+        return pdu;
     }
 
     boolean isBarrier() {
@@ -45,10 +96,6 @@ final class OutboundQueueEntry {
 
     boolean isCompleted() {
         return completed;
-    }
-
-    OfHeader getMessage() {
-        return message;
     }
 
     boolean complete(final OfHeader response) {
@@ -83,4 +130,8 @@ final class OutboundQueueEntry {
         }
     }
 
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this).add("message", message).toString();
+    }
 }
