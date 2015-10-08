6e8f8cf977654ec4198104591ea55d5d869311c8
Vaclav Demcak <vdemcak@cisco.com>
2015-10-08 04:51:23 +0200

    Bug 4432 - NPE problem in OFEncode
    * add check for listener in msg wrapper
    * add default listener for logging in OutboundQueueManager
    
    Change-Id: Ib652d60a210feaddf96840334af3b435e7d0a14f
    Signed-off-by: Vaclav Demcak <vdemcak@cisco.com>

--- openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/core/OFEncoder.java
+++ openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/core/OFEncoder.java
@@ -12,7 +12,6 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
 import io.netty.util.concurrent.Future;
-
 import org.opendaylight.openflowjava.protocol.impl.core.connection.MessageListenerWrapper;
 import org.opendaylight.openflowjava.protocol.impl.serialization.SerializationFactory;
 import org.opendaylight.openflowjava.statistics.CounterEventTypes;
@@ -28,31 +27,33 @@ import org.slf4j.LoggerFactory;
  */
 public class OFEncoder extends MessageToByteEncoder<MessageListenerWrapper> {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(OFEncoder.class);
+    private static final Logger LOG = LoggerFactory.getLogger(OFEncoder.class);
     private SerializationFactory serializationFactory;
-    private StatisticsCounters statisticsCounters;
+    private final StatisticsCounters statisticsCounters;
 
     /** Constructor of class */
     public OFEncoder() {
         statisticsCounters = StatisticsCounters.getInstance();
-        LOGGER.trace("Creating OF13Encoder");
+        LOG.trace("Creating OF13Encoder");
     }
 
     @Override
-    protected void encode(ChannelHandlerContext ctx, MessageListenerWrapper wrapper, ByteBuf out)
+    protected void encode(final ChannelHandlerContext ctx, final MessageListenerWrapper wrapper, final ByteBuf out)
             throws Exception {
-        LOGGER.trace("Encoding");
+        LOG.trace("Encoding");
         try {
             serializationFactory.messageToBuffer(wrapper.getMsg().getVersion(), out, wrapper.getMsg());
             if(wrapper.getMsg() instanceof FlowModInput){
                 statisticsCounters.incrementCounter(CounterEventTypes.DS_FLOW_MODS_SENT);
             }
             statisticsCounters.incrementCounter(CounterEventTypes.DS_ENCODE_SUCCESS);
-        } catch(Exception e) {
-            LOGGER.warn("Message serialization failed ", e);
+        } catch(final Exception e) {
+            LOG.warn("Message serialization failed ", e);
             statisticsCounters.incrementCounter(CounterEventTypes.DS_ENCODE_FAIL);
-            Future<Void> newFailedFuture = ctx.newFailedFuture(e);
-            wrapper.getListener().operationComplete(newFailedFuture);
+            if (wrapper.getListener() != null) {
+                final Future<Void> newFailedFuture = ctx.newFailedFuture(e);
+                wrapper.getListener().operationComplete(newFailedFuture);
+            }
             out.clear();
             return;
         }
@@ -61,7 +62,7 @@ public class OFEncoder extends MessageToByteEncoder<MessageListenerWrapper> {
     /**
      * @param serializationFactory
      */
-    public void setSerializationFactory(SerializationFactory serializationFactory) {
+    public void setSerializationFactory(final SerializationFactory serializationFactory) {
         this.serializationFactory = serializationFactory;
     }
 
