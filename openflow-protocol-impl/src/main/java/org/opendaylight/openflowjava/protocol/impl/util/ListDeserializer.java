4b69afb27f842dc9d013be1ab79127068bfc4959
Ryan Goulding <ryandgoulding@gmail.com>
2016-08-17 04:06:09 -0400

    Reduce missing table feature log message from warn to debug level
    Change-Id: I5b4441c9e1e78b414bba081d8df67d389d872d35
    Signed-off-by: Ryan Goulding <ryandgoulding@gmail.com>

--- openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/util/ListDeserializer.java
+++ openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/util/ListDeserializer.java
@@ -84,11 +84,9 @@ public final class ListDeserializer {
                     // are not yet (2nd February 2016) fully supported by existing OF Plugin.
                     // TODO - simplify to correctly report exception during deserialization
                     if (!exceptionLogged) {
-                        LOG.warn("Problem during reading table feature property. Skipping unknown feature property: {}." +
-                                "If more information is needed, set org.opendaylight.openflowjava do DEBUG log level.",
-                                key, e.getMessage());
                         if (LOG.isDebugEnabled()) {
-                            LOG.debug("Detailed exception: {}", e);
+                            LOG.debug("Problem during reading table feature property. Skipping unknown feature property: {}." +
+                                    key, e);
                             LOG.debug("This exception is logged only once for each multipart reply (table features) to "
                                     + "prevent log flooding. There might be more of table features related exceptions.");
                         }
