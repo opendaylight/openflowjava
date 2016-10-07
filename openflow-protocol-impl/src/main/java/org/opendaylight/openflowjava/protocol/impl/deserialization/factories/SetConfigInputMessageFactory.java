3ae7f47f9e46520bebac1c16f725623160328f18
Andrej Leitner <andrej.leitner@pantheon.tech>
2016-10-07 16:57:49 +0200

    Change GetConfigReq/Res and SetConfig factories to version assignable
     - made SetConfig, GetConfigRequest and GetConfigReply factories
       version assignable
     - removed OF10 factories duplicates
     - registered new deserializers
     - update tests
    
    Resolves: Bug 4255
    
    Change-Id: Ib4809c58d62aa4608d88894d01c6d107d2fff90c
    Signed-off-by: Andrej Leitner <andrej.leitner@pantheon.tech>

--- openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/deserialization/factories/SetConfigInputMessageFactory.java
+++ openflow-protocol-impl/src/main/java/org/opendaylight/openflowjava/protocol/impl/deserialization/factories/SetConfigInputMessageFactory.java
@@ -10,20 +10,22 @@ package org.opendaylight.openflowjava.protocol.impl.deserialization.factories;
 import io.netty.buffer.ByteBuf;
 import org.opendaylight.openflowjava.protocol.api.extensibility.OFDeserializer;
 import org.opendaylight.openflowjava.protocol.api.util.EncodeConstants;
+import org.opendaylight.openflowjava.protocol.impl.util.VersionAssignableFactory;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.common.types.rev130731.SwitchConfigFlag;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.SetConfigInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.SetConfigInputBuilder;
 
 /**
+ * Translates SetConfig messages.
+ * OF protocol versions: 1.0, 1.3, 1.4, 1.5.
  * @author giuseppex.petralia@intel.com
- *
  */
-public class SetConfigInputMessageFactory implements OFDeserializer<SetConfigInput> {
+public class SetConfigInputMessageFactory extends VersionAssignableFactory implements OFDeserializer<SetConfigInput> {
 
     @Override
     public SetConfigInput deserialize(ByteBuf rawMessage) {
         SetConfigInputBuilder builder = new SetConfigInputBuilder();
-        builder.setVersion((short) EncodeConstants.OF13_VERSION_ID);
+        builder.setVersion(getVersion());
         builder.setXid(rawMessage.readUnsignedInt());
         builder.setFlags(SwitchConfigFlag.forValue(rawMessage.readUnsignedShort()));
         builder.setMissSendLen(rawMessage.readUnsignedShort());
