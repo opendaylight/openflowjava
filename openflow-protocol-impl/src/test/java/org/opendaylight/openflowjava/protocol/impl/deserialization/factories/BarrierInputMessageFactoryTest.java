6505f1cd53248a4ebe0d6959c19a94f16243d447
Andrej Leitner <andrej.leitner@pantheon.tech>
2016-10-10 09:44:38 +0200

    Change BarrierReq/Res factories to version assignable
      - made BarrierRequest/BarrierReply factories version assignable
      - removed OF10 factories duplicates
      - registered new deserializers
      - update tests
      - fix typo
    
    Resolves: Bug 4255
    
    Change-Id: I248941b94ea6974679a27313cd6634baaa0fc034
    Signed-off-by: Andrej Leitner <andrej.leitner@pantheon.tech>

--- openflow-protocol-impl/src/test/java/org/opendaylight/openflowjava/protocol/impl/deserialization/factories/BarrierInputMessageFactoryTest.java
+++ openflow-protocol-impl/src/test/java/org/opendaylight/openflowjava/protocol/impl/deserialization/factories/BarrierInputMessageFactoryTest.java
@@ -8,35 +8,46 @@
 package org.opendaylight.openflowjava.protocol.impl.deserialization.factories;
 
 import io.netty.buffer.ByteBuf;
-import org.junit.Before;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import org.junit.Test;
-import org.opendaylight.openflowjava.protocol.api.extensibility.DeserializerRegistry;
-import org.opendaylight.openflowjava.protocol.api.extensibility.OFDeserializer;
 import org.opendaylight.openflowjava.protocol.api.keys.MessageCodeKey;
 import org.opendaylight.openflowjava.protocol.api.util.EncodeConstants;
-import org.opendaylight.openflowjava.protocol.impl.deserialization.DeserializerRegistryImpl;
 import org.opendaylight.openflowjava.protocol.impl.util.BufferHelper;
+import org.opendaylight.openflowjava.protocol.impl.util.DefaultDeserializerFactoryTest;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.openflow.protocol.rev130731.BarrierInput;
 
 /**
+ * Test for {@link org.opendaylight.openflowjava.protocol.impl.deserialization.factories.BarrierInputMessageFactory}.
  * @author giuseppex.petralia@intel.com
- *
  */
-public class BarrierInputMessageFactoryTest {
-    private OFDeserializer<BarrierInput> factory;
+public class BarrierInputMessageFactoryTest extends DefaultDeserializerFactoryTest<BarrierInput> {
 
-    @Before
-    public void startUp() {
-        DeserializerRegistry desRegistry = new DeserializerRegistryImpl();
-        desRegistry.init();
-        factory = desRegistry
-                .getDeserializer(new MessageCodeKey(EncodeConstants.OF13_VERSION_ID, 20, BarrierInput.class));
+
+    /**
+     * Initializes deserializer registry and lookups OF13 deserializer.
+     */
+    public BarrierInputMessageFactoryTest() {
+        super(new MessageCodeKey(EncodeConstants.OF13_VERSION_ID, 20, BarrierInput.class));
     }
 
+    /**
+     * Testing of {@link BarrierInputMessageFactory} for correct header version.
+     */
     @Test
-    public void test() {
+    public void testVersions() {
+        List<Byte> versions = new ArrayList<>(Arrays.asList(
+                EncodeConstants.OF13_VERSION_ID,
+                EncodeConstants.OF14_VERSION_ID,
+                EncodeConstants.OF15_VERSION_ID
+        ));
         ByteBuf bb = BufferHelper.buildBuffer();
-        BarrierInput deserializedMessage = BufferHelper.deserialize(factory, bb);
-        BufferHelper.checkHeaderV13(deserializedMessage);
+        testHeaderVersions(versions, bb);
+
+        // OFP v1.0 need to be tested separately cause of different message type value
+        messageCodeKey = new MessageCodeKey(EncodeConstants.OF10_VERSION_ID, 18, BarrierInput.class);
+        testHeaderVersions(Collections.singletonList(EncodeConstants.OF10_VERSION_ID), bb);
     }
 }
